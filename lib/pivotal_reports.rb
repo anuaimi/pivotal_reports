72b59dc9d8599fcb7b2a9c20f48f12c993da5b27
Athir Nuaimi <athir@nuaimi.com>
2014-09-16 06:21:41 -0400

    add basic Rakefile

--- lib/pivotal_reports.rb
+++ lib/pivotal_reports.rb
@@ -1,4 +1,11 @@
 
+__LIB_DIR__ = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift __LIB_DIR__ unless $LOAD_PATH.include?(__LIB_DIR__)
+
+require 'tracker_api'
+
+require 'pivotal_reports/version'
+
 module PivotalReports
 
   class CLI
@@ -6,5 +13,6 @@ module PivotalReports
       puts "Well, hi there!"
     end
   end
+
 end
 
